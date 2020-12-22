167f9fd61fcb12de9f7a0cbf8c40537c7eba2dda
Maksim1108 <maksim.lyanko@bk.ru>
2020-12-22 17:33:48 +0600

    create connection form

--- js/index.js
+++ js/index.js
@@ -23,6 +23,8 @@ result.addEventListener('click', () => {
     result.classList.toggle('active')
 })
 
+//================================================
+
 let circle1 = document.getElementById('circle-1')
 let circle2 = document.getElementById('circle-2')
 let circle3 = document.getElementById('circle-3')
@@ -38,9 +40,15 @@ let infoBlock5 = document.getElementById('infoBlock-5')
 let circles = [circle1, circle2, circle3, circle4, circle5]
 let infoBlocks = [infoBlock1, infoBlock2, infoBlock3, infoBlock4, infoBlock5]
 
-circles.forEach((items, i) => items.addEventListener('click', () => {
-    circles[i].classList.toggle('active')
-    infoBlocks[i].classList.toggle('active')
+circles.forEach((items, index) => items.addEventListener('click', () => {
+    // circles[index].classList.toggle(circles[index].classList == 'active' ? '' : 'active')
+    // infoBlocks[index].classList.toggle('active')
+    if(circles[index].classList == 'active'){
+        circles[index].classList.remove('active')
+    }
+    else{
+        circles[index].classList.add('active')
+    }
 }))
 
 // ===============================================
@@ -52,4 +60,37 @@ const searchIcon = document.querySelector('.header__icon')
 
 searchIcon.addEventListener('click', () => {
     searchInput.classList.toggle('active')
+})
+
+// ==================================
+
+const body = document.body
+const headerBtn = document.querySelector('.header__btn')
+const connection = document.querySelector('.header__connection')
+const connectionClose = document.querySelector('.form__close')
+
+headerBtn.addEventListener('click', () => {
+    connection.classList.add('active')
+    body.classList.add('active')
+})
+
+connectionClose.addEventListener('click', () => {
+    connection.classList.remove('active')
+    body.classList.remove('active')
+})
+
+//==============================================
+
+const footerNumber = document.querySelector('.footer__phone')
+const call = document.querySelector('.footer__call')
+const callClose = document.querySelector('.call__close')
+
+footerNumber.addEventListener('click', () => {
+    call.classList.add('active')
+    body.classList.add('active')
+})
+
+callClose.addEventListener('click', () => {
+    call.classList.remove('active')
+    body.classList.remove('active')
 })
